91962435c8efca71807a379c1d68a9d846331dbb
ahamad <ahamad@archis.company>
2021-08-20 16:48:23 +0530

    DB connection initiaized and User Register API created

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const connectDB = require("./config/db");
+
 
 const app = express();
 
-app.use('/', (req, res) => res.send('API Running'));
+// Connect to Database
+connectDB();
+
+//Init Middleware
+app.use(express.json({extended: false}));
+
+// app.use('/', (req, res) => res.send('API Running'));
+
+app.use('/api/posts', require("./routes/api/posts"));
+app.use('/api/auth', require("./routes/api/auth"));
+app.use('/api/profile', require("./routes/api/profile"));
+app.use('/api/users', require("./routes/api/users"));
 
 const PORT = process.env.PORT || 5000;
 
